f6dcd98f949ef53d25b716e308fd9bf98896a3ac
CooperLindquist <90310805@ep-student.org>
2024-04-22 10:00:48 -0500

    disfunctional changes

---
+++ ACSCJA/DBTest.swift
@@ -0,0 +1,18 @@
+//
+//  DBTest.swift
+//  ACSCJA
+//
+//  Created by 90310805 on 4/22/24.
+//
+
+import SwiftUI
+
+struct DBTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DBTest()
+}
---
+++ ACSCJA/Score.swift
@@ -0,0 +1,8 @@
+//
+//  Score.swift
+//  ACSCJA
+//
+//  Created by 90310805 on 4/22/24.
+//
+
+import Foundation
