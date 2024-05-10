62d68af7c62ab610aa2e7af4ced546f0548d8abe
CooperLindquist <90310805@ep-student.org>
2024-05-10 09:39:11 -0500

    poopy buttox2

---
+++ ACSCJA/ArchivedScoreView.swift
@@ -0,0 +1,18 @@
+//
+//  ArchivedScoreView.swift
+//  ACSCJA
+//
+//  Created by 90310805 on 5/7/24.
+//
+
+import SwiftUI
+
+struct ArchivedScoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArchivedScoreView()
+}
---
+++ ACSCJA/ArchivedScoreViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ArchivedScoreViewModel.swift
+//  ACSCJA
+//
+//  Created by 90310805 on 5/7/24.
+//
+
+import Foundation
