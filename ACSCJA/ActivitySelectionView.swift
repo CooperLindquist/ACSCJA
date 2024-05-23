feef4419d16c437eae28920b322bd64c44f607c3
CooperLindquist <90310805@ep-student.org>
2024-05-22 20:47:46 -0500

    big shit

---
+++ ACSCJA/ActivitySelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  ActivitySelectionView.swift
+//  ACSCJA
+//
+//  Created by 90310805 on 5/22/24.
+//
+
+import SwiftUI
+
+struct ActivitySelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivitySelectionView()
+}
---
+++ ACSCJA/ActivityViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ActivityViewModel.swift
+//  ACSCJA
+//
+//  Created by 90310805 on 5/22/24.
+//
+
+import Foundation
