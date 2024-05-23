63098e6e6755f2f382c04ba75327fe4c2a9282f5
CooperLindquist <90310805@ep-student.org>
2024-05-22 22:10:53 -0500

    YES!

---
+++ ACSCJA/AdminScoresView.swift
@@ -0,0 +1,18 @@
+//
+//  AdminScoresView.swift
+//  ACSCJA
+//
+//  Created by 90310805 on 5/22/24.
+//
+
+import SwiftUI
+
+struct AdminScoresView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdminScoresView()
+}
---
+++ ACSCJA/AdminTabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  AdminTabBarView.swift
+//  ACSCJA
+//
+//  Created by 90310805 on 5/22/24.
+//
+
+import SwiftUI
+
+struct AdminTabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdminTabBarView()
+}
