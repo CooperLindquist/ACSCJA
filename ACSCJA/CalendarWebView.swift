2c41d1edfd9d417b89eea0c5ea9d359f83652621
Cooper Lindquist <90310805@ep-student.org>
2024-06-17 01:36:56 -0500

    idek

---
+++ ACSCJA/CalendarWebView.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarWebView.swift
+//  ACSCJA
+//
+//  Created by Cooper Lindquist on 6/15/24.
+//
+
+import SwiftUI
+
+struct CalendarWebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalendarWebView()
+}
---
+++ ACSCJA/NamePromptView.swift
@@ -0,0 +1,18 @@
+//
+//  NamePromptView.swift
+//  ACSCJA
+//
+//  Created by Cooper Lindquist on 6/17/24.
+//
+
+import SwiftUI
+
+struct NamePromptView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NamePromptView()
+}
